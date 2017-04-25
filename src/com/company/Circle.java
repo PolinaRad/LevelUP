6efe076f53397541731ebeef01f46f0d72dfb698
Polina <polina.radchencko@gmail.com>
2017-04-25 14:10:31 +0300

    init commit proj2

---
+++ src/com/company/Circle.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Circle
+{
+    double x;
+    double y;
+    double r;
+    Circle(double x, double y, double r)
+    {
+        this.x =x;
+        this.y=y;
+        this.r=r;
+        //r = Math.pow(x*x+y*y,0.5);
+
+    }
+
+
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Circle c = new Circle(2,3,4);
+        double area = Math.PI*Math.pow(c.r,2);
+        System.out.println("area = " + area);
+        double in;
+       Scanner.class;
+        }
+}
+
+      ///  System.out.println("r = " + r);
+        //System.out.println("x = " + r);
+        //System.out.println("y = " + r);

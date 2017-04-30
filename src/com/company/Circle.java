24335d7357bd93dce2c11d2070c64038ab5ef9d9
Polina <polina.radchencko@gmail.com>
2017-04-30 22:05:35 +0300

    init commit proj2

--- src/com/company/Circle.java
+++ src/com/company/Circle.java
@@ -1,18 +1,103 @@
 package com.company;
+import java.util.Scanner;
+
+import static java.lang.Math.PI;
+import static java.lang.Math.pow;
+
 
 public class Circle
 {
     double x;
     double y;
     double r;
-    Circle(double x, double y, double r)
+    private double nr;
+
+
+    Circle()
+      {
+          System.out.println("Введите координаты точки:");
+          Scanner scanner = new Scanner(System.in);
+          double x = scanner.nextDouble();
+          double y = scanner.nextDouble();
+          this.x =x;
+          this.y=y;
+          System.out.println("Введите радиус окружности:");
+          double r = scanner.nextDouble();
+          this.r=r;
+          //r = Math.pow(x*x+y*y,0.5);
+  
+      }
+     
+ /* Circle( double r)
+  {
+      this.x=0;
+      this.y=0;
+      this.r=r;
+      //r = Math.pow(x*x+y*y,0.5);
+
+  }*/
+
+    void move ()
+    { System.out.println("Введите изменения координат центра:");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("dx:");
+        double dx = scanner.nextDouble();
+        System.out.println("dy:");
+        double dy = scanner.nextDouble();
+        x+=dx;
+        y+=dy;
+
+    }
+
+    double area ()
     {
-        this.x =x;
-        this.y=y;
-        this.r=r;
-        //r = Math.pow(x*x+y*y,0.5);
+        return PI*Math.pow(r,2);
 
     }
 
 
+    public void cahngeR() {
+        System.out.println("Введите на сколько увеличить радиус:");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("dr:");
+        double dr = scanner.nextDouble();
+        r += dr;
+
+    }
+
+    public void changeNR() {
+        System.out.println("Введите во раз сколько увеличить радиус:");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("NR:");
+        double nr = scanner.nextDouble();
+        r=Math.pow(r,nr);
+    }
+
+    public void checkPoint() {
+        System.out.println("Введите координаты точки:");
+        Scanner scanner = new Scanner(System.in);
+        double x1 = scanner.nextDouble();
+        double y1 = scanner.nextDouble();
+        if (((x1 - x) * (x1 - x) + (y1 - y) * (y1 - y)) < r * r) {
+            System.out.println("Точка принадлежит окружности.");
+        } else if (((x1 - x) * (x1 - x) + (y1 - y) * (y1 - y)) == r * r) {
+            System.out.println("Точка лежит на окружности.");
+        } else
+            System.out.println("Точка не принадлежит окружности.");
+
+    }
+
+    public double lengh() {
+        return 2*PI*r;
+    }
+
+    public double diameter() {
+        return 2*r;
+    }
+
+    public void about() {
+        System.out.println("Радиус: " + r);
+        System.out.println("Центр окружности по оси x: " +(x));
+        System.out.println("Центр окружности по оси y: " +(y));
+    }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,12 +7,31 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        Circle c = new Circle(2,3,4);
-        double area = Math.PI*Math.pow(c.r,2);
-        System.out.println("area = " + area);
-        double in;
-       Scanner.class;
-        }
+        Circle c = new Circle();
+        c.area();
+        System.out.println("area = " + c.area());
+
+
+       c.move();
+    //    System.out.println("c.x = " + c.x);
+      //  System.out.println("c.y = " + c.y);
+
+      //  c.cahngeR();
+        System.out.println("c.r = " + c.r);
+      //  c.changeNR();
+      //  System.out.println("c.r2 = " + c.r);
+
+      //  c.checkPoint();
+        c.lengh();
+      //  System.out.println("длина окружности = " + c.lengh());
+
+        c.diameter();
+    //    System.out.println("диаметр = " + c.diameter());
+        c.about();
+    }
+
+
+
 }
 
       ///  System.out.println("r = " + r);
